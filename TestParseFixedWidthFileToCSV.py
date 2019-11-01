7216ebc85c1d5a063a795578a2736d103f7a898b
hunter <hunter@bellroy.com>
2019-11-01 11:58:11 +1100

    Additional comments for TestParseFixedWidthFileToCSV.py

--- TestParseFixedWidthFileToCSV.py
+++ TestParseFixedWidthFileToCSV.py
@@ -1,3 +1,5 @@
+"""PyUnit tests for parsing fixed width files"""
+
 import unittest
 import os
 import csv
@@ -6,8 +8,17 @@ from ParseFixedWidthFileToCSV import _calculate_cumulative_offsets, _encode_list
 
 
 class TestInternalFunctions(unittest.TestCase):
+    """Unit tests for the internal helper functions"""
 
     def test_calculate_cumulative_offsets(self):
+        """Tests _calculate_cumulative_offsets
+           Has cases for:
+              offsets missing
+              negative offsets
+              non integer offsets
+              the spec offests
+        """
+
         with self.assertRaises(AssertionError):
             _calculate_cumulative_offsets([])
         with self.assertRaises(AssertionError):
@@ -21,6 +32,11 @@ class TestInternalFunctions(unittest.TestCase):
 
 
     def test_encode_list_items(self):
+        """Tests _encode_list_items
+           Has cases for:
+              windows-1252 to uft-8
+        """
+
         item1 = "item1".encode("windows-1252")
         item2 = "item2".encode("windows-1252")
         input_list = [item1,item2]
@@ -30,6 +46,12 @@ class TestInternalFunctions(unittest.TestCase):
         self.assertEquals(input_list, output_list)
 
     def test_create_padding(self):
+        """Tests _create_padding
+           Has cases for:
+              2 stars
+              3 spaces
+        """
+
         expected_padding = "   "
         created_padding = _create_padding(3)
         self.assertEqual(expected_padding, created_padding)
@@ -37,10 +59,12 @@ class TestInternalFunctions(unittest.TestCase):
         created_padding= _create_padding(2, "*")
         self.assertEqual(expected_padding, created_padding)
 
-    """since this is internal and
-    used after assertions I assume valid input.
-    """
     def test_split_fixed_width_row(self):
+        """Tests _split_fixed_width_row
+           Has cases for:
+              valid input (internal function used behind assertions)
+        """
+
         row = "aaBBBBC\n"
         cumulative_offsets =[2,6,7]
         expected_split_row =["aa","BBBB","C"]
@@ -48,28 +72,41 @@ class TestInternalFunctions(unittest.TestCase):
         self.assertEqual(split_row, expected_split_row)
 
 class TestExternalFunction(unittest.TestCase):
-    
+    """Tests for parse_fixed_width_file_to_csv, effectively intergration tests"""
+
     def test_with_no_header(self):
+        """Tests that files with no header are rejected"""
+
         with self.assertRaises(ValueError):
             parse_fixed_width_file_to_csv("NoHeaderTestInput", "NoHeaderTestOutput")
 
     def test_with_windows_only_char(self):
+        """Tests that character not translatable to utf-8 cause and error"""
+
         with self.assertRaises(UnicodeDecodeError):
             parse_fixed_width_file_to_csv("WindowsTestInput", "WindowsOnlyTestOutput")
     
     def test_with_wrong_header(self):
+        """Tests that invalid headers are rejected"""
+
         with self.assertRaises(ValueError):
             parse_fixed_width_file_to_csv("WrongHeaderTestInput", "WrongHeaderTestOutput")
 
     def test_with_row_too_long(self):
+        """Tests that files with overlong rows are rejected"""
+
         with self.assertRaises(AssertionError):
             parse_fixed_width_file_to_csv("LongRowHeaderTestInput", "TooLongTestOutput")
 
     def test_with_row_too_short(self):
+        """Tests that files with too short rows are rejected"""
+
         with self.assertRaises(AssertionError):
             parse_fixed_width_file_to_csv("ShortRowTestInput", "TooTestShortOutput")
     
     def test_commas_in_input(self):
+        """Tests that commas in the input are properly esacped in the CSV"""
+
         parse_fixed_width_file_to_csv("CommaTestInput", "CommaTestOutput",
                                       column_names = ["f1"], offsets = [1])
         with open("CommaTestOutput", 'r') as output:
@@ -80,6 +117,8 @@ class TestExternalFunction(unittest.TestCase):
         os.remove("CommaTestOutput")
 
     def test_quotes_in_input(self):
+        """Test that quotes in the input are properly escaped in the CSV"""
+
         parse_fixed_width_file_to_csv("QuoteTestInput", "QuoteTestOutput",
                                       column_names = ["f1"], offsets = [2])
         with open("QuoteTestOutput", 'r') as output:
