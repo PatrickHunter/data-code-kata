b6395aa0ae7b21affa30e1b408adb460c5adf062
hunter <hunter@bellroy.com>
2019-11-01 11:21:11 +1100

    Additional comments for CreateDummyFixedWidthFile.py

--- CreateDummyFixedWidthFile.py
+++ CreateDummyFixedWidthFile.py
@@ -1,12 +1,17 @@
+"""Functions for use in creating the fixed width file"""
 import string
 import random
 from random import randint
 
 def create_padding(padding_length, padding_char =" "):
+    """generates padding to append to  short entries"""
+
     return padding_char * padding_length
 
 
 def create_fixed_width_header(column_names, offsets):
+    """Creates a string represnting the header row"""
+
     header = ""
     for x in range(0, len(column_names)):
         padding = create_padding(offsets[x] - len(column_names[x]))
@@ -16,12 +21,16 @@ def create_fixed_width_header(column_names, offsets):
     return header
 
 def create_dummy_entry(offset):
+    """Creates random string right padded with spaces and length equalt to offset"""
+
     padding_length = randint(0,offset)
     content = ''.join(random.choice(string.punctuation + string.ascii_uppercase) for _ in range(offset - padding_length))
     entry = content + create_padding(padding_length)
     return entry
 
 def create_dummy_row(offsets):
+    """Creates a string representation of a row, with entry's whose lengths match the offsets"""
+
     row = ""
     for offset in offsets:
         row += create_dummy_entry(offset)
@@ -30,6 +39,16 @@ def create_dummy_row(offsets):
 
 
 def create_dummy_fixed_width_file(column_names, offsets, num_rows, filename):
+    """Create a fixed width file with random data.
+
+    Args:
+        column_names: The names of the columns as a list of strings.
+        offests: The offests as a list of integers.
+        num_rows: The number of rows (excluding the header) as an integer.
+        filname: The name of the file to be writen.
+
+    """
+
     content = create_fixed_width_header(column_names, offsets)
     for x in range(0, num_rows):
         content += create_dummy_row(offsets)
